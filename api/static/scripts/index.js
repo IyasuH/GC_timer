feccc943af1894ed27306a58103b7f58a6ad3ef6
IyasuH <eyasuhailegbr@gmail.com>
2023-05-19 22:23:23 +0300

    updates

--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -1,6 +1,6 @@
 const countdown = () => {
     // Specify the date and time we are counting down to.
-    const countDate = new Date("Jun 17, 2023 00:00:00").getTime();
+    const countDate = new Date("Jul 8, 2023 00:00:00").getTime();
     const now = new Date().getTime();
     const remainingTime = countDate - now;
   
