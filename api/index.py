9d753d2d1cdd51b704276ed9d1c87e645334d5b5
IyasuH <eyasuhailegbr@gmail.com>
2023-05-28 23:18:44 +0300

    update

--- api/index.py
+++ api/index.py
@@ -1,10 +1,27 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+from deta import Deta
+
+load_dotenv()
+
+DETA_KEY=os.getenv("DETA_KEY")
+deta = Deta(DETA_KEY)
+
+last_word_db=deta.Base("last_word")
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    all_last_words = last_word_db.fetch().items
+    sorted=[]
+    for x in range(0, len(all_last_words), 3):
+        t=x+3
+        sorted.append(all_last_words[x:t])
+    print(sorted)
+    return render_template('home.html', datas=sorted)
 
 if __name__ == "__main__":
     app.run(debug=True, host="192.168.1.14", port=7070)
--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -48,9 +48,54 @@ function toTopBtn(){
     document.body.scrollTop=0;
     document.documentElement.scrollTop=0;
     if (document.body.scrollTop >20 || document.documentElement.scrollTop >20){
-        mybutton.style.display = "block";
+        mybutton.style.display = "flex";
     }else{
         mybutton.style.display="none";
     }
 
+}
+
+// 
+// This is for the image slide inside album
+var currentImg = 0;
+var imgs = document.querySelectorAll('.slider img');
+let dots = document.querySelectorAll('.dot');
+var interval = 3000;
+
+var timer = setInterval(changeSlide, interval);
+
+function changeSlide(n){
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].style.opacity = 0;
+        dots[i].className = dots[i].className.replace(' active', '');
+      }
+
+      currentImg = (currentImg + 1) % imgs.length;
+
+      if (n != undefined) {
+        clearInterval(timer);
+        timer = setInterval(changeSlide, interval);
+        currentImg = n;
+      }
+
+      imgs[currentImg].style.opacity = 1;
+      dots[currentImg].className += ' active';    
+}
+
+var intervall = 4500
+var timerr = setInterval(changeQuote, intervall);
+var currentQuote = 0;
+var quotes = document.querySelectorAll('.quotteGp')
+
+function changeQuote(n){
+    for (var i=0; i<quotes.length; i++){
+     quotes[i].style.opacity = 0;
+    }
+    currentQuote = (currentQuote + 1) % quotes.length;
+    if (n!=undefined){
+        clearInterval(timerr);
+        timerr = setInterval(changeQuote, intervall);
+        currentQuote = n;
+    }
+    quotes[currentQuote].style.opacity = 1;
 }
